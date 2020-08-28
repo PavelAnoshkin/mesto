9050a4927de5de6a69a9e0d99bfdb3ea27dfa9d4
Pavel Anoshkin <monitoring.tk-ekat@yandex.ru>
2020-08-28 22:54:13 +0500

    Hotfix: Правки по комментариям

--- script/script.js
+++ script/script.js
@@ -1,31 +1,37 @@
 const updateButton = document.querySelector('.profile__edit-button');
 const cancelButton = document.querySelector('.popup__btn_action_close');
 const dialogPopup = document.querySelector('.popup');
-
+const formElement = document.querySelector('.popup__form');
 let name = document.querySelector('.profile__title');
 let job = document.querySelector('.profile__subtitle');
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_description');
 
+function popupOpen(popup) {
+    popup.classList.toggle('popup_opened'); 
+}
 
-updateButton.addEventListener('click', function() {
+function formOpen() {
     nameInput.value = name.textContent;
     jobInput.value = job.textContent;
-    dialogPopup.classList.add('popup_opened');
-});
+    popupOpen(dialogPopup);
+}
 
-cancelButton.addEventListener('click', function() {
+function formClose() {
     nameInput.value = '';
     jobInput.value = '';
-    dialogPopup.classList.remove('popup_opened');
-});
-
-const formElement = document.querySelector('.popup__form');
-formElement.addEventListener('submit', formSubmitHandler);
+    popupOpen(dialogPopup);
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();    
     name.textContent = nameInput.value;
     job.textContent = jobInput.value;
-    dialogPopup.classList.remove('popup_opened');   
+    popupOpen(dialogPopup);   
 }
+
+updateButton.addEventListener('click', formOpen);
+
+cancelButton.addEventListener('click', formClose);
+
+formElement.addEventListener('submit', formSubmitHandler);
